4d03de55df63bec48b2bccdb3a3002ad650977e4
MALESELALETSOALO <emjaymalesela@gmail.com>
2022-06-09 12:06:18 +0200

    Add project files.

---
+++ days of the week.cpp
@@ -0,0 +1,47 @@
+// why we call it life program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   
+    int Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+    string Days;
+
+
+
+
+    cout << " Count days of the week " <<endl;
+    cin >> Days;
+
+
+    cout << " day 1:" << endl;
+    cin >> Monday;
+    cout << "day2" << endl;
+    cin >> Tuesday;
+    cout << "day3" << endl;
+    cin >> Wednesday;
+    cout << "day4" << endl;
+    cin >> Thursday;
+    cout << "day5" << endl;
+    cin >> Friday;
+    cout << "day6" << endl;
+    cin >> Saturday;
+    cout << "day7" << endl;
+    cin >> Sunday;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
